21fd53e8435f98819c47474a20a2dc49795c8fd7
Shresth Jain <theshresthjain@gmail.com>
2026-02-03 17:49:06 +0530

    Initial Commit

---
+++ myDay/myDayApp.swift
@@ -0,0 +1,32 @@
+//
+//  myDayApp.swift
+//  myDay
+//
+//  Created by Shresth Jain on 03/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct myDayApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
